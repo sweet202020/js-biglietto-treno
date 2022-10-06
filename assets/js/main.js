303ccbe54f7aa02e6de3d02c5a2c05c870906344
sweet202020 <dolcealessio1@gmail.com>
2022-10-06 15:56:54 +0200

    problem NaN

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,11 +8,28 @@ Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio,
 L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). */
 
 //chiedere il numero di chilometri che si vuole percorrere
-const NumberKm = prompt("quanti chilometri vuoi percorrere?")
+
+const NumberKm = Number(prompt("quanti chilometri vuoi percorrere?"));
 console.log(NumberKm);
+
 //chiedere l'eta del passeggero
-const userAge = prompt ("quanti anni hai?")
+
+const userAge = Number(prompt("quanti anni hai?"));
 console.log(userAge);
+
 //calcolare il prezzo del biglietto in base ai km
-const wholePrice = NumberKm * 0.21 
-console.log(wholePrice)
+
+const wholePrice = (NumberKm * 0.21 + '€');
+console.log(wholePrice);
+
+//fare sconto del 20 % per i minorenni utilizzndo if
+
+if (userAge<18) {
+    const teenDiscount = (wholePrice * 0.20);
+    const finalPriceTeen = (wholePrice - teenDiscount);
+    console.log(finalPriceTeen);
+} else if (userAge>65){
+    const overDiscount = (wholePrice * 0.40);
+    const finalOldPrice = (wholePrice - overDiscount);
+    console.log(finalOldPrice);
+}
